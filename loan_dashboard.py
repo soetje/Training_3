d9b08fa1ea230b58edc67452ca5a3e9a9f5a790e
soetje <r.c.m.soetekouw@gmail.com>
2026-05-14 21:56:14 +0200

    Increase margin-top of start button to 140px

--- loan_dashboard.py
+++ loan_dashboard.py
@@ -272,7 +272,7 @@ snake_html = """
   .ctrl-grid button:hover,
   .ctrl-grid button:active { background: #4ECDC4; color: #16213e; }
   #startBtn {
-    margin-top: 14px;
+    margin-top: 140px;
     padding: 10px 32px;
     font-size: 16px;
     border: none;
